a8cfcc3f4be366da8c913aeb00f2964bced5620e
Zaw <zawlinnnaing0018@gmail.com>
2026-02-11 16:43:37 +0700

    refactor: remove explore tab and clean up index screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,15 +39,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
